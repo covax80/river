fcb071487c7b85d2f93b1a2bc11268ebbd625d9b
danya <test@test.ru>
2022-01-30 23:31:41 +1000

    create models.py

--- riverproj/product/models.py
+++ riverproj/product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=1024)
+    def __str__(self):
+        return f"({self.pk}) {name}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=1024)
+    description = models.TextField()
+    def __str__(self):
+        return self.name
+
+class CategoryLink(models.Model):
+    product = models.ForeignKey(Product)
+    category = models.ForeignKey(Category)
+
